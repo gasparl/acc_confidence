086e2d57bf1d97d2b6e521bde4a3f724e29a20bf
Gaspar <lkcsgaspar@gmail.com>
2019-08-08 15:02:26 +0200

    responses stored

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var trial_num = 0;
 var trial_times = {};
 var subj_data = [
-    "subject_id", "trial_number", "category", "video", "decision", "confidence", "v_start", "v_end", "v_closed", "decision_time", "confidence_time", "date_in_ms\n"
+    "subject_id", "trial_number", "category", "video", "decision_first", "confidence_first", "decision_last", "confidence_last", "v_start", "v_end", "v_closed", "decision_time_first", "confidence_time_first", "decision_time_last", "confidence_time_last", "date_in_ms\n"
 ].join("\t");
 
 function store_start() {
@@ -41,6 +41,17 @@ function store_start() {
         });
 }
 
+
+["subject_id", "trial_number", "category", "video", "decision_first", "confidence_first", "decision_last", "confidence_last", "v_start", "v_end", "v_closed", "decision_time_first", "confidence_time_first", "decision_time_last", "confidence_time_last", "date_in_ms\n"
+].join("\t");
+
+function store_trial() {
+    subj_data += [
+        subj_id, trial_num, "category", vid_name, responses.main_first, responses.conf_first, responses.main_last, responses.conf_last, trial_times.v_start, trial_times.v_end, trial_times.v_closed, responses.main_rt_first, responses.conf_rt_first, responses.main_rt_last, responses.conf_rt_last, neat_date()
+    ].join("\t") + "\n";
+}
+
+
 function save_main() {
     var rt = now();
     if (responses.main_first === "-") {
@@ -59,13 +70,6 @@ function save_conf() {
     }
     responses.conf_last = $("#conf_rate_id").val();
     responses.conf_rt_last = rt;
-}
-
-function store_trial() {
-    var main_resp = $('input[name=main_decision]:checked').val();
-    subj_data += [
-        subj_id, trial_num, "category", vid_name, main_resp, "confidence", trial_times.v_start, trial_times.v_end, trial_times.v_closed, "decision_time", "confidence_time", neat_date()
-    ].join("\t") + "\n";
 }
 
 // video settings
